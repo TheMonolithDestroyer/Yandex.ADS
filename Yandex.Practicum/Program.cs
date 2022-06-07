2f843b88d15ea5790c0581c3e78acfe11a136a41
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-07 10:59:41 +0600

    Initial commit

---
+++ Yandex.Practicum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yandex.Practicum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

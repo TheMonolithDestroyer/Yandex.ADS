2fcac8dc00eeb3fc87a85d6bff8c00a8254a6c54
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-07 15:47:30 +0600

    Task2 of sprint0

--- Yandex.Practicum/Program.cs
+++ Yandex.Practicum/Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Yandex.Practicum
 {
     public class Program
     {
+        protected static TextReader _reader;
+        protected static TextWriter _writer;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private static int ReadInt()
+        {
+            return int.Parse(_reader.ReadLine());
+        }
+
+        private static List<int> ReadList()
+        {
+            return _reader.ReadLine()
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+        }
+
+        private static void InitReaderAndWriter()
+        {
+            _reader = new StreamReader(Console.OpenStandardInput());
+            _writer = new StreamWriter(Console.OpenStandardOutput());
+        }
+
+        private static void CloseReaderAndWriter()
+        {
+            _reader.Close();
+            _writer.Close();
         }
     }
 }
---
+++ Yandex.Practicum/Sprints/Sprint0/Task2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex.Practicum.Sprints.Sprint0
+{
+    public class Task2 : BaseClass
+    {
+        public static void MainTask2()
+        {
+            InitReaderAndWriter();
+
+            var n = Common.ReadInt(_reader);
+            var arr1 = Common.ReadList(_reader);
+            var arr2 = Common.ReadList(_reader);
+
+            int position = 0;
+            int[] result = new int[2 * n];
+            for (int i = 0; i < n; i++)
+            {
+                result[position] = arr1[i];
+                result[position + 1] = arr2[i];
+
+                position += 2;
+            }
+
+            _writer.WriteLine(string.Join(" ", result));
+
+            CloseReaderAndWriter();
+        }
+    }
+}

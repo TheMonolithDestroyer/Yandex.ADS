c613fc420644189b7f7e8e559098ed1c86dbe30a
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-07 11:21:01 +0600

    Task1 and delete suo file from repository

---
+++ Yandex.Practicum/BaseClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex.Practicum
+{
+    public class BaseClass
+    {
+        protected internal static TextReader _reader;
+        protected internal static TextWriter _writer;
+
+        internal protected virtual void InitReaderAndWriter()
+        {
+            _reader = new StreamReader(Console.OpenStandardInput());
+            _writer = new StreamWriter(Console.OpenStandardOutput());
+        }
+
+        internal protected virtual void CloseReaderAndWriter()
+        {
+            _reader.Close();
+            _writer.Close();
+        }
+    }
+}

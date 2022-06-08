314bd5a1723f11197f0ea7dc24a636e5adf7b0d6
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-09 03:01:16 +0600

    as

--- Yandex.Practicum/Sprints/Sprint1/BinarySearch.cs
+++ Yandex.Practicum/Sprints/Sprint1/BinarySearch.cs
@@ -19,43 +19,69 @@ namespace Yandex.Practicum.Sprints.Sprint1
 
             Array.Sort(numbers, left, right + 1);
 
-            var result = GetFoundValueIndex();
+            _writer.WriteLine("");
+            _writer.WriteLine("После сортировки");
+            _writer.WriteLine(string.Join(' ', numbers));
 
-            _writer.WriteLine(result);
+            _writer.WriteLine("");
+            _writer.WriteLine("Результат наивного алгоритма");
+            var naiveResult = NaiveBinarySearch(x, numbers, left, right);
+            _writer.WriteLine(naiveResult);
 
-            int GetFoundValueIndex()
-            {
-                if (x < numbers[left] || x > numbers[right])
-                    return -1;
-
-                if (right + 1 == 1 && numbers[left] == x)
-                    return left;
-
-                while(right - left > 1)
-                {
-                    var middle = left + (right - left) / 2;
-
-                    if (numbers[middle] == x)
-                        return middle;
-
-                    if (numbers[middle] > x)
-                        right = middle;
-                    else 
-                        left = middle;
-                }
-
-                if (right - left == 1)
-                {
-                    if (numbers[left] == x)
-                        return left;
-                    else
-                        return right;
-                }
-
-                return -1;
-            }
+            //_writer.WriteLine("");
+            //_writer.WriteLine("Результат оптимизированного алгоритма");
+            //var optimizedResult = OptimizedBinarySearch(x, numbers, left, right);
+            //_writer.WriteLine(optimizedResult);
 
             CloseReaderAndWriter();
+        }
+
+        //private static int OptimizedBinarySearch(int x, int[] numbers, int left, int right)
+        //{
+        //    if (x < numbers[left] || x > numbers[right])
+        //        return -1;
+
+        //    var iterationCount = Math.Log(right + 1, x);
+        //    var count = (int)Math.Round(iterationCount, MidpointRounding.AwayFromZero);
+
+        //    for (int i = 0; i < count; i++)
+        //    {
+
+        //    }
+
+        //    return -1;
+        //}
+
+        private static int NaiveBinarySearch(int x, int[] numbers, int left, int right)
+        {
+            if (x < numbers[left] || x > numbers[right])
+                return -1;
+
+            if (right + 1 == 1 && numbers[left] == x)
+                return left;
+
+            while (right - left > 1)
+            {
+                var middle = left + (right - left) / 2;
+
+                if (numbers[middle] == x)
+                    return middle;
+
+                if (numbers[middle] > x)
+                    right = middle;
+                else
+                    left = middle;
+            }
+
+            if (right - left == 1)
+            {
+                if (numbers[left] == x)
+                    return left;
+                else
+                    return right;
+            }
+
+            return -1;
         }
     }
 }

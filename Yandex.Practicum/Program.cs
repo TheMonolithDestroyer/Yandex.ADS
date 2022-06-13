8560cd511c49291a88181e442ccba6fe39677d43
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-14 00:10:22 +0600

    task8 of sprint1

--- Yandex.Practicum/Program.cs
+++ Yandex.Practicum/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
+using System.Text;
 
 namespace Yandex.Practicum
 {
@@ -12,26 +14,99 @@ namespace Yandex.Practicum
         {
             InitReaderAndWriter();
 
-            int number = ReadInt();
+            var firstBinary = ReadString();
+            var secondBinary = ReadString();
 
-            string result = string.Empty;
+            int i = firstBinary.Length - 1;
+            int j = secondBinary.Length - 1;
 
-            do
+            int reminder = 0;
+            Stack myStack = new();
+            while (i >= 0 || j >= 0)
             {
-                var reminder = number % 2;
-                number /= 2;
-                result = reminder + result;
-            }
-            while (number > 0);
+                char? a = i < 0 ? null : firstBinary[i];
+                char? b = j < 0 ? null : secondBinary[j];
 
-            _writer.WriteLine(result);
+                switch (a, b)
+                {
+                    case (null, '1'):
+                    case ('1', null):
+                        {
+                            myStack.Push(reminder > 0 ? '0' : '1');
+                        }
+                        break;
+                    case (null, '0'):
+                    case ('0', null):
+                        {
+                            if (reminder > 0)
+                            {
+                                myStack.Push('1');
+                                reminder -= 1;
+                            }
+                            else
+                            {
+                                myStack.Push('0');
+                            }
+                        }
+                        break;
+                    case ('0', '0'):
+                        {
+                            if (reminder > 0)
+                            {
+                                myStack.Push('1');
+                                reminder -= 1;
+                            }
+                            else
+                            {
+                                myStack.Push('0');
+                            }
+                        }
+                        break;
+                    case ('1', '0'):
+                    case ('0', '1'):
+                        {
+                            myStack.Push(reminder > 0 ? '0' : '1');
+                        }
+                        break;
+                    case ('1', '1'):
+                        {
+                            if (reminder > 0)
+                            {
+                                myStack.Push('1');
+                            }
+                            else
+                            {
+                                myStack.Push('0');
+                                reminder += 1;
+                            }
+                        }
+                        break;
+                }
+
+                i--;
+                j--;
+            }
+
+            if (reminder > 0)
+            {
+                myStack.Push('1');
+            }
+
+            StringBuilder sb = new(); 
+            
+            while(myStack.Count > 0)
+            {
+                sb.Append(myStack.Pop());
+            }
+
+            _writer.WriteLine(sb.ToString());
 
             CloseReaderAndWriter();
         }
 
-        private static int ReadInt()
+        public static string ReadString()
         {
-            return int.Parse(_reader.ReadLine());
+            return _reader.ReadLine();
         }
 
         private static void InitReaderAndWriter()
---
+++ Yandex.Practicum/Sprints/Sprint1/Task8.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex.Practicum.Sprints.Sprint1
+{
+    public class Task8 : BaseClass
+    {
+        public static void MainTask8()
+        {
+            InitReaderAndWriter();
+
+            // < 10^5
+            var firstBinary = Common.ReadString(_reader);
+            var secondBinary = Common.ReadString(_reader);
+
+            int i = firstBinary.Length - 1;
+            int j = secondBinary.Length - 1;
+            
+            int reminder = 0;
+            Stack myStack = new();
+            while (i >= 0 || j >= 0)
+            {
+                var a = firstBinary[i];
+                var b = secondBinary[j];
+
+                switch (a, b)
+                {
+                    case ('0', '0'):
+                        {
+                            if (reminder > 0)
+                            {
+                                myStack.Push('1');
+                                reminder -= 1;
+                            }
+                            else
+                            {
+                                myStack.Push('0');
+                            }
+                        }
+                        break;
+                    case ('1', '0'):
+                    case ('0', '1'):
+                        {
+                            myStack.Push(reminder > 0 ? '0' : '1');
+                        }
+                        break;
+                    case ('1', '1'):
+                        {
+                            if (reminder > 0)
+                            {
+                                myStack.Push('1');
+                            }
+                            else
+                            {
+                                myStack.Push('0');
+                                reminder += 1;
+                            }
+                        }
+                        break;
+                }
+
+                i--;
+                j--;
+            }
+
+            if (reminder > 0)
+            {
+                myStack.Push('1');
+            }
+
+            _writer.WriteLine(string.Join(string.Empty, myStack.ToArray()));
+
+            CloseReaderAndWriter();
+        }
+    }
+}

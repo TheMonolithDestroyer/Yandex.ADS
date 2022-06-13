4ae1cd53c16c28a2965e2160dc1084f7ba0abee2
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-13 11:38:02 +0600

    Task1 and Task2 of sprint1

---
+++ Yandex.Practicum/Sprints/Sprint1/Task1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex.Practicum.Sprints.Sprint1
+{
+    public class Task1 : BaseClass
+    {
+        public static void MainTask1()
+        {
+            InitReaderAndWriter();
+
+            var numbers = Common.ReadArray(_reader);
+
+            var result = (numbers[0] * Math.Abs(numbers[1] * numbers[1])) + (numbers[2] * numbers[1]) + numbers[3];
+
+            _writer.WriteLine(result);
+
+            CloseReaderAndWriter();
+        }
+    }
+}
---
+++ Yandex.Practicum/Sprints/Sprint1/Task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex.Practicum.Sprints.Sprint1
+{
+    public class Task2 : BaseClass
+    {
+        public static void MainTask2()
+        {
+            InitReaderAndWriter();
+
+            var numbers = Common.ReadArray(_reader);
+
+            var a = numbers[0] % 2 == 0;
+            var b = numbers[1] % 2 == 0;
+            var c = numbers[2] % 2 == 0;
+
+            if ((!a && !b && !c) || (a && b && c))
+                _writer.WriteLine("WIN");
+            else
+                _writer.WriteLine("FAIL");
+
+            CloseReaderAndWriter();
+        }
+    }
+}

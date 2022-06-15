b442e47ecc3817300b098f606f4ed15214afd35c
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-16 04:44:29 +0600

    Final task1 of sprint1

--- Yandex.Practicum/Program.cs
+++ Yandex.Practicum/Program.cs
@@ -1,53 +1,81 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Yandex.Practicum
 {
-    public class Program
+	// ID = 68983618
+	public class Program
     {
         private static TextReader _reader;
         private static TextWriter _writer;
 
         static void Main(string[] args)
         {
-            InitReaderAndWriter();
+			InitReaderAndWriter();
 
-            int listFormLength = ReadInt();
-            string listForm = ReadString();
-            int kNumber = ReadInt();
+			int streetLength = ReadInt();
+			int[] blocks = ReadArray();
 
-            string concatedListForm = string.Empty;
-            for (int i = 0; i < listForm.Length; i++)
-            {
-                if (listForm[i] != ' ')
+			int emptyBlock = -1;
+			int nextEmptyBlock = -1;
+
+			int[] result = new int[streetLength];
+			for (int i = 0; i < streetLength; i++)
+			{
+				if (blocks[i] == 0)
+				{
+					result[i] = 0;
+					emptyBlock = i;
+					nextEmptyBlock = -1;
+					continue;
+				}
+
+				int j = i;
+				while (nextEmptyBlock < 0 && j < blocks.Length)
                 {
-                    concatedListForm += listForm[i];
+					if (blocks[j] == 0)
+                    {
+						nextEmptyBlock = j;
+						break;
+                    }
+
+					j++;
                 }
-            }
 
-            int xNumber = Convert.ToInt32(concatedListForm);
-            int numberResult = xNumber + kNumber;
+				int distance = 0;
+				if (emptyBlock >= 0 && nextEmptyBlock < 0)
+				{
+					distance = i - emptyBlock;
+				}
+				else if (emptyBlock < 0 && nextEmptyBlock >= 0)
+				{
+					distance = nextEmptyBlock - i;
+				}
+				else
+				{
+					int leftDistance = i - emptyBlock;
+					int rightDistance = nextEmptyBlock - i;
+					distance = leftDistance > rightDistance ? rightDistance : leftDistance;
+				}
 
-            string result = string.Empty;
-            while (numberResult > 0)
-            {
-                var middleNumber = numberResult % 10;
-                numberResult /= 10;
+				result[i] = distance;
+			}
 
-                result = middleNumber.ToString() + ' ' + result;
-            }
+			_writer.WriteLine(string.Join(' ', result));
 
-            _writer.WriteLine(result.TrimEnd());
+			CloseReaderAndWriter();
+		}
 
-            CloseReaderAndWriter();
-        }
+		private static int[] ReadArray()
+		{
+			return _reader.ReadLine()
+				.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+		}
 
-        public static string ReadString()
-        {
-            return _reader.ReadLine();
-        }
-
-        public static int ReadInt()
+		private static int ReadInt()
         {
             return int.Parse(_reader.ReadLine());
         }
---
+++ Yandex.Practicum/Sprints/Sprint1/FinalTask1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex.Practicum.Sprints.Sprint1
+{
+    public class FinalTask1 : BaseClass
+    {
+		// 1) [0, 1, 4, 9, 0] = [0, 1, 2, 1, 0]
+		// 2) [0, 7, 9, 4, 8, 20] = [0, 1, 2, 3, 4, 5]
+		// 3) [0] = [0]
+		// 4) [0, 1] = [0, 1]
+		// 5) [0, 0, 0, 0, 0] = [0, 0, 0, 0, 0]
+		// 6) [0, 0, 1, 0, 0] = [0, 0, 1, 0, 0]
+		// 7) [1, 3, 5, 7, 0, 2, 4, 6, 8, 9] = [4, 3, 2, 1, 0, 1, 2, 3, 4, 5]
+		// 8) [1, 0, 10^4, 0, 10^9, 10^7, 2, 5, 0] = [1, 0, 1, 0, 1, 2, 2, 1, 0]
+		// 9) [999, 888, 454, 19, 2, 64, 72, 0] = [7, 6, 5, 4, 3, 2, 1, 0]
+		// 10) [0, 1, 0, 5, 0, 10, 0, 91] = [0, 1, 0, 1, 0, 1, 0, 1]
+
+		public static void Execute()
+		{
+			InitReaderAndWriter();
+
+			int streetLength = Common.ReadInt(_reader);
+			int[] blocks = Common.ReadArray(_reader);
+
+			//List<int> emptyBlocks = new List<int>();
+			//for (int i = 0; i < streetLength; i++)
+			//	if (blocks[i] == 0)
+			//		emptyBlocks.Add(i);
+
+			int? emptyBlock = null;
+			int[] result = new int[streetLength];
+			for (int i = 0; i < streetLength; i++)
+			{
+				if (blocks[i] == 0)
+				{
+					result[i] = 0;
+					emptyBlock = i;
+					continue;
+				}
+
+				int? nextEmptyBlock = null;
+				for (int j = i; j < blocks.Length; j++)
+				{
+					if (blocks[j] != 0)
+						continue;
+
+					nextEmptyBlock = blocks[j];
+				}
+
+				int distance = 0;
+				if (emptyBlock != null && nextEmptyBlock == null)
+                {
+					distance = i - emptyBlock.Value;
+                }
+				else if (emptyBlock == null && nextEmptyBlock != null)
+                {
+					distance = nextEmptyBlock.Value - i;
+                }
+                else
+                {
+					int leftDistance = i - emptyBlock.Value;
+					int rightDistance = nextEmptyBlock.Value - i;
+					distance = leftDistance > rightDistance ? rightDistance : leftDistance;
+                }
+
+				result[i] = distance;
+				//int? distance = null;
+				//foreach (var j in emptyBlocks)
+				//{
+				//	var nextDistance = i - j;
+				//	if (nextDistance < 0)
+				//		nextDistance *= -1;
+
+				//	if (!distance.HasValue || (distance.HasValue && distance > nextDistance))
+				//	{
+				//		distance = nextDistance;
+				//	}
+				//}
+
+				//result[i] = distance.Value;
+			}
+
+			_writer.WriteLine(string.Join(' ', result));
+
+			CloseReaderAndWriter();
+		}
+	}
+}

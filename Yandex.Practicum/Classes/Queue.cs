2a933d765c0303077227f6fb5b9ec0cbbbe7cd7a
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2022-06-26 18:24:59 +0600

    Created custom queue class

---
+++ Yandex.Practicum/Classes/Queue.cs
@@ -0,0 +1,66 @@
+using System;
+using Yandex.Exceptions;
+
+namespace Yandex.Practicum.Classes
+{
+    public class Queue
+    {
+        private int _head;
+        private int _tail;
+        private int _count;
+        private int _maxN;
+        public int Count { get => _count; private set => _count = value; }
+        private int[] _queue = null;
+
+        public Queue(int size) 
+        {
+            InitQueue(size);
+
+            _head = 0;
+            _tail = 0;
+            _maxN = size;
+            _count = 0;
+        }
+
+        public void Push(int item)
+        {
+            if (_count >= _maxN) 
+                throw new OverflowException(message: "You can not insert an item into the full queue");
+            
+            _queue[_tail] = item;
+            _tail = (_tail + 1) % _maxN;
+            _count++;
+        }
+
+        public int Pop()
+        {
+            if (_count <= 0) 
+                throw new UnderflowException("You can not pop an empty queue!");
+
+            int item = _queue[_head];
+            _queue[_head] = 0;
+            _head = (_head + 1) % _maxN;
+            _count++;
+
+            return item;
+        }
+
+        public int Peek()
+        {
+            if (_count <= 0) 
+                throw new UnderflowException("You can not peek an empty queue!");
+
+            int item = _queue[_head];
+
+            return item;
+        }
+
+        private void InitQueue(int size)
+        {
+            if (size <= 0) 
+                throw new ArgumentOutOfRangeException(message: "The size of a Queue can not be 0 or less", paramName: $"Input size: { size }");
+
+            _queue = new int[size];
+        }
+    }
+}
